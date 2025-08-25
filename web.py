b8a700b5e076ae62d4d0a684634973081c68ec17
Thao NDT <thaondt4@gmail.com>
2025-08-25 11:01:49 +0700

    update web

--- web.py
+++ web.py
@@ -3,5 +3,8 @@ app=Flask(__name__)
 @app.route("/",methods=["GET","POST"])
 def trang_chu():
 	return render_template("trangchu.html")
+@app.route("/dang_nhap")
+def trang_chu_dang_nhap():
+	return render_template("dangnhap.html")
 if __name__==("__main__"):
 	app.run()
