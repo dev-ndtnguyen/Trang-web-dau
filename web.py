2685b9c93acf8add8cc4772f310343926a0b0e60
Thao NDT <thaondt4@gmail.com>
2025-08-27 08:20:53 +0700

    update

--- web.py
+++ web.py
@@ -18,19 +18,20 @@ def trang_chu_dang_ky():
 		username=request.form.get("ten")
 		password=request.form.get("mk")
 		try:
-			ket_noi=sqlite3.connect("dangky.db")
+			ket_noi=sqlite3.connect("dangky.db",timeout=15)
 			ghi=ket_noi.cursor()
 			ghi.execute("INSERT INTO dang_ky (username,password) VALUES (?,?)",(username,password))
 			ket_noi.commit()
-			ket_noi.close()
 			thanh_cong="ban da dang ky thanh cong , ban co the dang nhap tai khoan nay o phia duoi de truy cap trang web"
 			return render_template("dangnhap.html",thanh_cong=thanh_cong)
 		except sqlite3.IntegrityError:
 			that_bai="ten tai khoan ban nhap da co nguoi su dung"
 			return render_template("dangky.html",that_bai=that_bai)
+		finally :
+			ket_noi.close()
 	return render_template("dangky.html")
 @app.route("/dang_nhap",methods=["GET","POST"])
 def dang_nhap():
-	return template("dangnhap.html")
+	return render_template("dangnhap.html")
 if __name__==("__main__"):
 	app.run()
